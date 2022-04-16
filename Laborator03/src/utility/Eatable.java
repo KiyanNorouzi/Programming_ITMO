b4216402b738f509556d09301d9bcb99a7110a01
Kiyan Norouzi <41071501+KiyanNorouzi@users.noreply.github.com>
2022-04-16 17:18:49 +0300

    Delete Laborator03 directory

--- Laborator03/src/utility/Eatable.java
+++
@@ -1,5 +0,0 @@
-package utility;
-
-public interface Eatable {
-	void eat(String _name, String _food);
-}
--- Laborator03/src/utility/Objects.java
+++
@@ -1,6 +0,0 @@
-package utility;
-
-public enum Objects {
-    ADAPTED,
-    NON_ADAPTED;
-}
